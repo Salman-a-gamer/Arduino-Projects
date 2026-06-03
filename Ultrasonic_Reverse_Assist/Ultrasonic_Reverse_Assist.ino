d3b1edd9f7574949a904f81d6782a09f7d7a5e5d
Salman Imran Syed <68333931+Salman-a-gamer@users.noreply.github.com>
2026-06-03 15:02:30 +0530

    Rename passive buzzer variable and Added comments
    Just added some comments :)

--- Ultrasonic_Reverse_Assist/Ultrasonic_Reverse_Assist.ino
+++ Ultrasonic_Reverse_Assist/Ultrasonic_Reverse_Assist.ino
@@ -1,16 +1,18 @@
+// Install the Ultrasonic library by Eric Simoes
+
 #include <Ultrasonic.h>
-Ultrasonic u(10,8);
+Ultrasonic u(10,8);  // Using module, u(trig, echo)
 byte greenpin = 12;
 byte yellowpin = 5;
 byte redpin = 4;
-byte passbuz = 11;
+byte passive_buzzer = 11;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(greenpin, OUTPUT);
   pinMode(yellowpin, OUTPUT);
   pinMode(redpin, OUTPUT);
-  pinMode(passbuz, OUTPUT);
+  pinMode(passive_buzzer, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -20,26 +22,30 @@ void loop() {
   digitalWrite(yellowpin, LOW);
   digitalWrite(redpin, LOW);
 
-  int d = u.read();
+  int d = u.read();   // read in cm by default
+  
+  // If you want to check the distnce uncomment the following lines
   // Serial.print("Distance is "); 
   // Serial.print(d);
   // Serial.println(" cm");
 
+  // Range for yellow led in cm
   if(d >= 11 && d <=26){
     digitalWrite(greenpin, LOW);
     digitalWrite(redpin, LOW);
     digitalWrite(yellowpin , HIGH);
-    tone(passbuz, 500);
+    tone(passive_buzzer, 500);
   }
-  else if(d <=10){
+  else if(d <=10){   // range for red led in cm
     digitalWrite(greenpin, LOW);
     digitalWrite(yellowpin, LOW);
     digitalWrite(redpin, HIGH);
-    tone(passbuz, 1500);
+    tone(passive_buzzer, 1500);
   }
   else{
-  digitalWrite(greenpin, HIGH);
-  noTone(passbuz);
+  digitalWrite(greenpin, HIGH);     // Green led is always on by default
+  noTone(passive_buzzer);
   }
+  // Set the delay after every scan (milliseconds)
  delay(500);
 }
